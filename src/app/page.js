afac2098272689a50119e2e761284cad971558f2
nazmul910 <islamnasir910@gmail.com>
2026-04-10 22:49:42 +0600

    project structure added

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import { AnimatePresence } from "framer-motion";
 import Preloader from "../components/Preloader/index"
 import Landing from "../components/Landing/index"
 import Description from "../components/Description/index"
+import Projects from "../components/Projects/index"
 
 export default function Home() {
 
@@ -33,6 +34,7 @@ export default function Home() {
         </AnimatePresence>
         <Landing/>
         <Description/>
+        <Projects/>
 
 
       </main>
