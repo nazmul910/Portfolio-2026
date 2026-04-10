a9dbf5172fb75a532e5e2eea94bfeb64f6d76911
nazmul910 <islamnasir910@gmail.com>
2026-04-11 00:25:04 +0600

    Add Contact component and implement sliding images with animations

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ import Landing from "../components/Landing/index"
 import Description from "../components/Description/index"
 import Projects from "../components/Projects/index"
 import SlideingImages from "../components/SlideingImages/index"
+import Contact from "../components/Contact/index"
 
 export default function Home() {
 
@@ -37,6 +38,7 @@ export default function Home() {
         <Description/>
         <Projects/>
         <SlideingImages/>
+        <Contact/>
 
 
       </main>
