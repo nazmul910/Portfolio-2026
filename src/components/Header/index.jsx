b2143759aeb04c69363e97b126b6248f6793a302
nazmul910 <islamnasir910@gmail.com>
2026-04-11 18:34:19 +0600

    Header setup

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useEffect,useLayoutEffect,useRef,useState } from "react";
+import styles from './style.module.scss';
+import { usePathname } from "next/navigation";
+import { AnimatePresence } from "framer-motion";
+import gsap from "gsap";
+import Nav from './nav/index';
+import { ScrollTrigger } from "gsap/all";
+import Rounded from '../../common/RoundedButton/index'
+import Magnetic from '../../common/Magnetic/index'
+
+export default function index(){
+
+}
